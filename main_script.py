5b7518c7e1a904dadce0afe5ebba7d98bb13ff9b
Yadnyawalk Tale <ytale@redhat.com>
2024-02-23 14:08:03 +0530

    concating download and scanning

--- main_script.py
+++ main_script.py
@@ -8,7 +8,7 @@ def lookup_in_manifest(package_name, manifest_tasklists):
     lookup_result = find_packages(package_name)
     for i in lookup_result:
         if i in manifest_tasklists:
-            print("=> {}: scan is already done! ".format(i))
+            print("=> {}: Scan is already done! ".format(i))
             return True
         else:
             return False
@@ -17,31 +17,29 @@ def read_config():
     with open('config.json') as config_file:
         return json.load(config_file)
 
-def download_packages(version_dir, package_names, manifest_tasklists):
+def download_and_scan_packages(version_dir, package_names, manifest_tasklists):
     excluded_packages = []
     exclude_text = "No src packages available for "
     os.chdir(version_dir)
     for package_name in package_names:
-        if not lookup_in_manifest(package_name, manifest_tasklists):
+        if lookup_in_manifest(package_name, manifest_tasklists):
+            continue
+        else:
             download_command = ['brew', 'download-build', '--noprogress', '--arch=src',package_name]
             output = subprocess.run(download_command, capture_output=True, text=True).stdout
             if exclude_text in output: # gathering list of packages which are not downloaded
                 excluded_packages.append(output.split(exclude_text)[1])
+            # scan_command = [
+            # 'osh-cli', 'mock-build', '--priority=0',
+            # '--comment={}'.format(scan_id),
+            # "./{}".format(package_name)
+            # ]
+            # subprocess.run(scan_command)
 
     if excluded_packages: # printing if there is list has excluded packages
         print("=> Following packages were not able to get downloaded:\n")
         for package_name in excluded_packages:
             print(package_name)
-
-def scan_packages(version_dir, package_names, random_hash):
-    os.chdir(version_dir)
-    for package_name in package_names:
-        scan_command = [
-            'osh-cli', 'mock-build', '--priority=0',
-            '--comment={}'.format(random_hash),
-            "./{}".format(package_name)
-        ]
-        subprocess.run(scan_command)
 
 def find_packages(package_name, latest=False):
     scan_command = ['osh-cli', 'find-tasks', "--states=CLOSED", "--latest", "{}".format(package_name)]
@@ -58,23 +56,21 @@ def find_packages(package_name, latest=False):
 
 def main():
     config_data = read_config()
-    random_hash = hashlib.sha256(os.urandom(32)).hexdigest()[:6]
+    scan_id = hashlib.sha256(os.urandom(32)).hexdigest()[:6]
     create_manifest(config_data['rpm_extensions'])
     manifest_tasklists = get_manifest()
     original_dir = os.getcwd()
-    versions = config_data['versions']
+    brew_tags = config_data['brew_tags']
 
-    for version in versions:
+    for version in brew_tags:
         version_dir = os.path.join(original_dir, version)
         os.makedirs(version_dir, exist_ok=True)
-        
+
         list_command = ['brew', 'latest-pkg', '--all', version]
         command_output = subprocess.run(list_command, capture_output=True, text=True)
         output_lines = command_output.stdout.strip().split('\n')[2:]
         package_names = [line.split()[0] for line in output_lines]
-
-        download_packages(version_dir, package_names, manifest_tasklists)
-        #scan_packages(version_dir, package_names, random_hash)
+        download_and_scan_packages(version_dir, package_names, manifest_tasklists)
         os.chdir(original_dir)
 
 if __name__ == "__main__":
