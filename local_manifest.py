c2345de129005687682c3b3b168b7f1bc50f23a4
Yadnyawalk Tale <ytale@redhat.com>
2024-03-01 16:02:11 +0530

    generate report

--- local_manifest.py
+++ local_manifest.py
@@ -1,11 +1,11 @@
 import os
 import subprocess
 
-FILENAME = "manifest.txt"
+MANIFEST_FILENAME = "manifest.txt"
 
 def get_manifest():
     manifest_tasklists = []
-    with open(FILENAME, 'a+') as file:
+    with open(MANIFEST_FILENAME, 'a+') as file:
         file.seek(0)  # Set the file pointer to the beginning
         for line in file:
             manifest_tasklists.append(int(line.strip()))
@@ -16,7 +16,7 @@ def create_manifest(extensions):
         scan_command = ['osh-cli', 'find-tasks', '--states=CLOSED','-r', ext]
         output = subprocess.run(scan_command, capture_output=True, text=True).stdout
         numbers = [int(x) for x in output.split("\n") if x.strip()]
-        with open(FILENAME, 'a') as manifest_file:
+        with open(MANIFEST_FILENAME, 'a') as manifest_file:
             for number in numbers:
                 manifest_file.write(str(number) + '\n')
-    print("=> Manifest file has been created at {}".format(os.path.abspath(FILENAME)))
+    print("=> Manifest file has been created at {}".format(os.path.abspath(MANIFEST_FILENAME)))
--- main_script.py
+++ main_script.py
@@ -5,11 +5,14 @@ import hashlib
 from local_manifest import get_manifest, create_manifest
 from package_action import find_packages
 
+TASK_LIST = []
+
 def lookup_in_manifest(package_name, manifest_tasklists):
     lookup_result = find_packages(package_name)
     for i in lookup_result:
         if i in manifest_tasklists:
             print("=> {}: Scan is already done! ".format(i))
+            TASK_LIST.append(i)
             return True
         else:
             return False
@@ -18,7 +21,7 @@ def read_config():
     with open('config.json') as config_file:
         return json.load(config_file)
 
-def download_and_scan_packages(version_dir, package_names, manifest_tasklists):
+def download_and_scan_packages(version_dir, package_names, manifest_tasklists, scan_id):
     excluded_packages = []
     exclude_text = "No src packages available for "
     os.chdir(version_dir)
@@ -30,17 +33,25 @@ def download_and_scan_packages(version_dir, package_names, manifest_tasklists):
             output = subprocess.run(download_command, capture_output=True, text=True).stdout
             if exclude_text in output: # gathering list of packages which are not downloaded
                 excluded_packages.append(output.split(exclude_text)[1])
-            # scan_command = [
-            # 'osh-cli', 'mock-build', '--priority=0',
-            # '--comment={}'.format(scan_id),
-            # "./{}".format(package_name)
-            # ]
-            # subprocess.run(scan_command)
+            scan_command = [
+            'osh-cli', 'mock-build', '--priority=0',
+            '--comment={}'.format(scan_id),
+            "./{}".format(package_name)
+            ]
+            scanned_output = subprocess.run(scan_command)
+            print(scanned_output)
 
     if excluded_packages: # printing if there is list has excluded packages
         print("=> Following packages were not able to get downloaded:\n")
         for package_name in excluded_packages:
             print(package_name)
+    
+def generate_report():
+    os.makedirs("reports", exist_ok=True)
+    print(TASK_LIST)
+    for taskid in TASK_LIST:
+        download_command = ["osh-cli", "download-results", {taskid}, "-d", "reports"]
+        subprocess.run(download_command, capture_output=True, text=True).stdout
 
 def main():
     config_data = read_config()
@@ -58,7 +69,7 @@ def main():
         command_output = subprocess.run(list_command, capture_output=True, text=True)
         output_lines = command_output.stdout.strip().split('\n')[2:]
         package_names = [line.split()[0] for line in output_lines]
-        download_and_scan_packages(version_dir, package_names, manifest_tasklists)
+        download_and_scan_packages(version_dir, package_names, manifest_tasklists, scan_id)
         os.chdir(original_dir)
     config_data["scan_id"] = scan_id
 
