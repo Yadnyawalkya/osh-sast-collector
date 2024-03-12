a7e9bdbd27273bfd3b70351e2a875191096d78bf
Yadnyawalk Tale <ytale@redhat.com>
2024-03-12 22:17:42 +0530

    update generating report part

--- generate_report.py
+++ generate_report.py
@@ -1,72 +1,63 @@
-from package_action import get_package_list, find_package
-from local_manifest import create_manifest, get_manifest, lookup_in_manifest
 import json
 import os
 import subprocess
+import tarfile
+
+from local_manifest import create_manifest, get_manifest
+from package_action import find_package, get_package_list
 
 TASK_LIST = []
+parent_dir = "reports"
 
 with open('config.json') as config_file:
     config_data = json.load(config_file)
 brew_tags = config_data['brew_tags']["osp17.1"]
-original_dir = os.getcwd()
-parent_dir = "reports"
+
 if not os.path.exists(parent_dir):
     os.makedirs(parent_dir)
+
 create_manifest(config_data['related_comments'])
 manifest_tasklists = get_manifest()
 
+# Iterate over brew tags
 for version in brew_tags:
     version_dir = os.path.join(parent_dir, version)
-    os.makedirs(version_dir)
-    package_names = get_package_list(version)[0:5]
+    os.makedirs(version_dir, exist_ok=True)
+    package_names = get_package_list(version)[:5]  # Limit to first 5 packages
     for package_name in package_names:
         taskid = find_package(package_name)
-        print(taskid)
-        if ((taskid is not None) and taskid in manifest_tasklists):
-            print("=> {}: scan found! ".format(taskid))
-            download_command = ["osh-cli", f"download-results {taskid}", f"-d {version_dir}"]
-            output = subprocess.run(download_command, check=True)
+        if taskid is not None and taskid in manifest_tasklists:
+            print("=> {}: Scan found!".format(taskid))
+            download_command = ["osh-cli", "download-results", str(taskid), "-d", version_dir]
+            output = subprocess.run(download_command)
             print(output)
-            print("=> {}: scan downloded! ".format(taskid))
+            print("=> {}: Scan downloaded!".format(taskid))
             TASK_LIST.append(taskid)
 
-import ipdb;ipdb.set_trace()
-    
-    
+# Function to extract scan results and handle errors
+def extract_scan_results_err(file_name, tar_file_path, core_dir, dep_dir):
+    with tarfile.open(tar_file_path, 'r:xz') as tar:
+        for member in tar.getmembers():
+            if 'scan-results.err' in os.path.basename(member.name):
+                with tar.extractfile(member) as extracted_file:
+                    contents = extracted_file.read().decode('utf-8')
+                    if contents:
+                        contents = f'RESULT_FILE: {file_name}\n{contents}'  # First part
+                        contents = contents.replace('\n\nError: ', '\n\nRESULT_FILE: {}\nError: '.format(file_name))  # Middle part
+                        contents += '\n'  # Last part
+                        output_dir = core_dir if any(package in file_name for package in config_data['core_packages']) else dep_dir
+                        with open(os.path.join(output_dir, 'meta-scan-results.err'), 'w') as output_file:
+                            output_file.write(contents)
 
-
-
-# # todo
-
-# config_data = read_config()
-# scan_id = config_data['scan_id']
-# core_packages = config_data['scan_id']
-
-# if scan_id:
-#     #print that generating report from comment value = <scan_id>
-#     #execute this command: osh-cli find-tasks --comment --states=CLOSED <scan_id> 
-#     # store above command output into some variable
-# else:
-#     #there is no value set to scan_id of config.json which means report can not be generated, if you want generate report, please specify valid comment value
-
-# <create-temp-folder>
-# <create-core-folder> and store its absolute path in variables
-# <create-others-folder> and store its absolute path in variable
-
-# run this command to download result:
-# osh-cli download-results <number> -d <temp-folder>
-
-# change directory to <temp-folder>
-
-# if downloaded result name contains anything from core_packages:
-#     then move that result file to core-folder
-# else:
-#     then move that file into temp folder
-
-
-# make sure temp folder is  empty and delete it
-# change directory to core folder
-# create empty mega-scan-results-imp.err file
-# untar all the files one by one and append result of scan-results-imp.err into mega-scan-results-imp.err file 
-
+# Process files in subdirectories
+sub_dirs = os.listdir(parent_dir)
+for subdir in sub_dirs:
+    subdir_path = os.path.join(parent_dir, subdir)
+    for file_name in os.listdir(subdir_path):
+        if file_name.endswith(".tar.xz"):
+            file_path = os.path.join(subdir_path, file_name)
+            core_dir = os.path.join(parent_dir, 'core_results_{}'.format(subdir))
+            dep_dir = os.path.join(parent_dir, 'dep_results_{}'.format(subdir))
+            os.makedirs(core_dir, exist_ok=True)
+            os.makedirs(dep_dir, exist_ok=True)
+            extract_scan_results_err(file_name, file_path, core_dir, dep_dir)
