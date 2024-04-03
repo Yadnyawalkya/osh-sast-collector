8ac032b1c488ee3f5a878449068b686b673ca2a3
Yadnyawalk Tale <ytale@redhat.com>
2024-04-03 17:19:58 +0530

    updating multithreading on main_script

--- main_script.py
+++ main_script.py
@@ -9,48 +9,42 @@ import shutil
 from datetime import datetime
 import logging
 
+excluded_packages = []
+
 def read_config():
     with open('config.json') as config_file:
         return json.load(config_file)
-
-def download_and_scan_package(version_dir, package_names):
-    excluded_packages = []
-    exclude_text = "No src packages available for "
-    manifest_tasklists = get_manifest()
+    
+def scan_packages(version_dir, file_name):
     os.chdir(version_dir)
-    for package_name in package_names:
-        if lookup_in_manifest(package_name, manifest_tasklists):
-            continue
-        else:
-            os.chdir(version_dir)
-            download_command = ['brew', 'download-build', '--noprogress', '--arch=src', package_name]
-            output = subprocess.run(download_command, capture_output=True, text=True).stdout
-            if exclude_text in output: # gathering list of packages which are not downloaded
-                excluded_packages.append(output.split(exclude_text)[1])
-            for file_name in os.listdir():
-                #TODO: Add failover mechanism if OSH auth is not provided: only give not scanned package names
-                #TODO: Remove hardcoding of mock-config later
-                if ".el8ost" in file_name:
-                    mock_config = "rhos-rhel-8-x86_64"
-                else:
-                    mock_config = "rhos-rhel-9-x86_64"
-                #TODO: If package name does not got rpm extension, assuming it is podified container
-                scan_id = "openstack"
-                if (".el8ost" or ".el9ost") not in file_name:
-                    scan_id = "openstack-podified"
-                scan_command = [
-                'osh-cli', 'mock-build', '--priority=0', '--nowait',
-                '--comment={}'.format(scan_id), 
-                "--config={}".format(mock_config),
-                "./{}".format(file_name)
-                ]
-                scanned_output = subprocess.run(scan_command)
-                print(scanned_output)
+    if ".el8ost" in file_name:
+        mock_config = "rhos-rhel-8-x86_64"
+    else:
+        mock_config = "rhos-rhel-9-x86_64"
+    #TODO: If package name does not got rpm extension, assuming it is podified container
+    scan_id = "openstack"
+    if (".el8ost" or ".el9ost") not in file_name:
+        scan_id = "openstack-podified"
+    scan_command = [
+    'osh-cli', 'mock-build', '--priority=0', '--nowait',
+    '--comment={}'.format(scan_id), 
+    "--config={}".format(mock_config),
+    "./{}".format(file_name)
+    ]
+    scanned_output = subprocess.run(scan_command)
+    print(scanned_output)
 
-    if excluded_packages: # printing if there is list has excluded packages
-        print("=> Following packages were not able to get downloaded:\n")
-        for package_name in excluded_packages:
-            print(package_name)
+def download_packages(version_dir, manifest_tasklists, package_name):
+    exclude_text = "No src packages available for "
+    os.chdir(version_dir)
+    if lookup_in_manifest(package_name, manifest_tasklists):
+        continue
+    else:
+        os.chdir(version_dir)
+        download_command = ['brew', 'download-build', '--noprogress', '--arch=src', package_name]
+        output = subprocess.run(download_command, capture_output=True, text=True).stdout
+        if exclude_text in output: # gathering list of packages which are not downloaded
+            excluded_packages.append(output.split(exclude_text)[1])
 
 def main():
     # dir structure
@@ -66,6 +60,7 @@ def main():
     create_manifest(config_data['related_comments'])
     #TODO: take argument later which decides wheather to scan 17.1 or 18 or both
     all_tags = config_data['brew_tags']
+    manifest_tasklists = get_manifest()
 
     for brew_tags in all_tags:
         for version in brew_tags:
@@ -74,9 +69,26 @@ def main():
             #TODO: Add dynamic way to handling comment aka scan_id
             # scan_id = "{}-{}".format(version, hashlib.sha256(os.urandom(32)).hexdigest()[:6])
             package_names = get_package_list(version)
+
             with ThreadPoolExecutor(max_workers=200) as executor:
-                executor.submit(download_and_scan_package, version_dir, package_names)
+                futures = []
+                for package_name in package_names:
+                    futures.append(executor.submit(download_packages, version_dir, manifest_tasklists, file_name))           
+                for future in futures:
+                    future.result()
+
+            with ThreadPoolExecutor(max_workers=200) as executor:
+                futures = []
+                for file_name in os.listdir():
+                    futures.append(executor.submit(scan_packages(version_dir, file_name)))
+                for future in futures:
+                    future.result()
             os.chdir(root_dir)
+
+    if excluded_packages: # printing if there is list has excluded packages
+        print("=> Following packages were not able to get downloaded:\n")
+        for package_name in excluded_packages:
+            print(package_name)
     shutil.rmtree(root_dir)
 
 if __name__ == "__main__":
